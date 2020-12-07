4fa8a0e8a9e49268fe9aaeb034e1a3d694c3c947
TheFlappyPatty <75271177+TheFlappyPatty@users.noreply.github.com>
2020-12-07 09:37:11 -0800

    I finished all the UI Scripting.

---
+++ Assets/Ron/UI Scripts/Startscript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Startscript : MonoBehaviour
+{
+    public string LoadLevelname;
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(LoadLevelname);
+    }
+}
