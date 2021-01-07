70019130bf3fb2bedfac776dcf5764d110c6dd7f
TheFlappyPatty <75271177+TheFlappyPatty@users.noreply.github.com>
2021-01-07 09:09:27 -0800

    Added a box that will close the game

---
+++ Assets/Ron/UI Scripts/quitgamescript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitgamescript : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Application.Quit();
+    }
+}
