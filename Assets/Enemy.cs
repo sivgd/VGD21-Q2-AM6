b4ab01a6b6be4a0154cb9c99e8779da0f465fa27
ender <ender@PROXY.local>
2021-01-18 17:35:23 -0800

    Update TMPLevel1.unity, Enemy.cs, and 13 more files...

---
+++ Assets/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float xcl;
+    public float SpeedCap;
+    private Rigidbody2D BodyBoi;
+
+
+    void Start()
+    {
+        BodyBoi = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        
+
+        
+
+        if (BodyBoi.velocity.x > SpeedCap)
+        {
+            BodyBoi.velocity = new Vector2(SpeedCap, BodyBoi.velocity.y);
+        }
+        if (BodyBoi.velocity.x < -SpeedCap)
+        {
+            BodyBoi.velocity = new Vector2(-SpeedCap, BodyBoi.velocity.y);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D wall)
+    {
+        if(wall.tag == "Bounding1")
+        {
+            
+
+            BodyBoi.AddForce(new Vector2(-xcl, 0));
+        }
+
+        if (wall.tag == "Bounding2")
+        {
+            
+
+            BodyBoi.AddForce(new Vector2(xcl, 0));
+        }
+    }
+
+   
+}
