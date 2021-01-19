c0b8080b4569d58ca9819a8acfe188f2018bb087
TheFlappyPatty <75271177+TheFlappyPatty@users.noreply.github.com>
2021-01-19 10:18:29 -0800

    warmth system

---
+++ Assets/Ron/UI Scripts/Freeze.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Freeze : MonoBehaviour
+{
+    public Slider warmthbar;
+    public float heatlevel;
+
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            warmthbar.value  -= 1;
+        }
+    }
+     void OnCollisionStay2D(Collision2D collision)
+    {
+        warmthbar.value -= 1;
+    }
+}
---
+++ Assets/Ron/UI Scripts/Warmthbar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Warmthbar : MonoBehaviour
+{
+    public Slider warmthbar;
+     void OnCollisionStay2D(Collision2D collision)
+    {
+        warmthbar.value += 0.1f;
+    }
+}
