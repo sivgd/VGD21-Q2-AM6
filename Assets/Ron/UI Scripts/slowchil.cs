1901b82e4dee9b269f4e81519385ef5144270715
TheFlappyPatty <75271177+TheFlappyPatty@users.noreply.github.com>
2021-01-20 14:17:53 -0800

    stuff

---
+++ Assets/Ron/UI Scripts/slowchil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class slowchil : MonoBehaviour
+{
+    public Slider degrading;
+    void Update()
+    {
+        degrading.value -=  .005f;
+    }
+}
